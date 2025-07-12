6a8b5e7e6eae797a847bac3256e6100b26e8c364
MuhammadTayyab <af9874161@gmail.com>
2025-07-12 10:20:48 +0500

    CLI Runner.run_sync agent

---
+++ chatbot.py
@@ -0,0 +1,50 @@
+import os
+from dotenv import find_dotenv, load_dotenv
+import chainlit as cl
+from agents import AsyncOpenAI, OpenAIChatCompletionsModel, RunConfig, Agent, Runner
+
+
+load_dotenv(find_dotenv())
+gemini_api_key = os.getenv("GEMINI_API_KEY")
+
+
+# STEP#1
+provider = AsyncOpenAI(
+    api_key=gemini_api_key,
+    base_url="https://generativelanguage.googleapis.com/v1beta/openai/" 
+)
+
+#STEP#2
+model = OpenAIChatCompletionsModel(
+    model="gemini-2.0-flash", 
+    openai_client=provider
+)
+
+
+#STEP#3
+run_config = RunConfig(
+    model=model,
+    model_provider=provider,
+    tracing_disabled = True,
+)
+
+
+
+#STEP#4
+agent1 = Agent(
+    name="Tayyab Agent",
+    instructions="You are a helpful agent."
+)
+
+
+user_input = input("Enter Query: ")
+
+result = Runner.run_sync(
+    agent1,
+    input=user_input,
+    run_config=run_config
+)
+
+print(result.final_output)
+
+
